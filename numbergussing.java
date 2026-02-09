7c41e30d8cbe626a8bd9e9ba1a22f3d4a41ffb2e
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-09 17:53:56 +0530

    update

---
+++ numbergussing.java
@@ -0,0 +1,12 @@
+public class numbergussing {
+    public static void main(String[] args) {
+        Random rand = new random();
+        int number = rand nextInt(100)+1;
+        Scanner sc = new Scanner (System.in);
+        int guess = 0;
+
+
+
+
+    }
+}
