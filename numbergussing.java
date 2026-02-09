1a8a1ef2716dd2deecd73e0a7660412d1e315c9e
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-09 19:50:37 +0530

    creating number gussing game

--- numbergussing.java
+++ numbergussing.java
@@ -1,12 +1,30 @@
-public class numbergussing {
+  import java.util.Random;
+  import java.util.*;
+   
+   public class numbergussing {
     public static void main(String[] args) {
-        Random rand = new random();
-        int number = rand nextInt(100)+1;
-        Scanner sc = new Scanner (System.in);
-        int guess = 0;
+    Random rand = new Random();
+    int number = rand.nextInt(100)+1;
+    System.out.println("Welcome to the Number gussing game !");
+        Scanner sc = new Scanner(System.in);
+    int guess = sc.nextInt();
+    System.out.print("Select number between 1 to 100 :");
+
+
+ int attempts=0;
 
 
 
+while (guess != number) {
+            guess = sc.nextInt();
+            attempts++;
 
+            if (guess > number)
+                System.out.println("Too High!");
+            else if (guess < number)
+                System.out.println("Too Low!");
+            else
+                System.out.println("Correct! Attempts: " + attempts);
+        }
     }
 }
