3062b7f65bfe780dd4a54e222cee24b6807fbd51
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-24 18:06:57 +0530

    class que

---
+++ classque.java
@@ -0,0 +1,12 @@
+ public class classque {
+    String city = "Indore";
+    int population = 90000000;
+    
+    public static void main(String[] args) {
+        classque c = new classque();
+        classque p = new classque();
+        System.out.println(c.city);
+        System.out.println(p.population);
+
+    }
+} 
