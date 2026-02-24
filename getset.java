d7266ec3c0d603780a548014ff16cd774c570e10
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-24 19:17:57 +0530

    update getset.java

--- getset.java
+++ getset.java
@@ -1,5 +1,17 @@
 public class getset {
+
+    private String name="YUVRAJ";
+    public String getname(){
+        return name;
+    }
+        public void setname(String name){
+            this.name = name;
+
+        }
+
     public static void main(String[] args) {
-        
+            getset obj = new getset();
+            System.out.println(obj.getname());
+
     }
 }
