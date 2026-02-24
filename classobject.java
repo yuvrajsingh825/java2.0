fe1f6089e6cd3cb221dbdaf033496b4184c0ac69
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-24 17:48:02 +0530

    classobject.java created

---
+++ classobject.java
@@ -0,0 +1,11 @@
+public class classobject {
+    int car = 10;
+    char name ='y';
+
+    public static void main(String[] args) {
+        classobject obj = new classobject();
+        classobject obj2 = new classobject();
+        System.out.println(obj.car);
+        System.out.println(obj2.name);
+    }
+}
