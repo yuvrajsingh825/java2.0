b6ad39c7f8956990905379a1ccda25c751317762
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-02-24 18:09:26 +0530

    update classobject.java

--- classobject.java
+++ classobject.java
@@ -1,11 +1,18 @@
 public class classobject {
-    int car = 10;
+    int car= 10;
     char name ='y';
+      final float PI = 3.14f;
 
     public static void main(String[] args) {
         classobject obj = new classobject();
         classobject obj2 = new classobject();
+        classobject obj3 = new classobject();
+
+         obj2.car = 3441;
         System.out.println(obj.car);
         System.out.println(obj2.name);
+        System.out.println(obj3.PI);
+
+
     }
 }
--- numbergussing.java
+++ numbergussing.java
@@ -6,16 +6,12 @@
     Random rand = new Random();
     int number = rand.nextInt(100)+1;
     System.out.println("Welcome to the Number gussing game !");
-        Scanner sc = new Scanner(System.in);
-    int guess = sc.nextInt();
     System.out.print("Select number between 1 to 100 :");
-
-
- int attempts=0;
-
-
-
-while (guess != number) {
+    Scanner sc = new Scanner(System.in);
+    int guess = sc.nextInt();
+  
+    int attempts=5;
+while (guess != number && attempts > 0) {
             guess = sc.nextInt();
             attempts++;
 
