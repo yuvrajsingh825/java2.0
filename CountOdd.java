8af1651c5212c76aee0e0b9bdd88b2e632f987f2
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-10 18:38:47 +0530

    aount

---
+++ CountOdd.java
@@ -0,0 +1,14 @@
+public class CountOdd {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for(int i = 1; i <= 10; i++){
+            if(i % 2 != 0){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
