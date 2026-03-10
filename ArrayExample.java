15f347b065b117ae3b93428b8f5c179b2a323d9c
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-10 19:07:46 +0530

    update

--- ArrayExample.java
+++ ArrayExample.java
@@ -10,5 +10,17 @@ public class ArrayExample {
         System.out.println(numbers[0]);
         System.out.println(numbers[1]);
         System.out.println(numbers[2]);
+
+        int[] arr = {5,2,9,1,7};
+
+int max = arr[0];
+
+for(int i = 1; i < arr.length; i++){
+    if(arr[i] > max){
+        max = arr[i];
+    }
+}
+
+System.out.println(max);
     }
 }
